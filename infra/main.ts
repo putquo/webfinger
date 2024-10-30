d40697b77940a23fe9724a8c1a61de99a8191068
Preston van Tonder <46090392+putquo@users.noreply.github.com>
2024-10-30 14:37:33 +0100

    feat: add infrastructure

---
+++ infra/main.ts
@@ -0,0 +1,34 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as cloudflare from "@pulumi/cloudflare";
+import * as std from "@pulumi/std";
+
+const config = new pulumi.Config();
+
+const script = new cloudflare.WorkersScript("script", {
+    accountId: config.require("accountId"),
+    name: "webfinger",
+    content: std.file({
+        input: "../dist/main.js",
+    }).then(invoke => invoke.result),
+    module: true,
+});
+
+const domain = new cloudflare.WorkersDomain("domain", {
+    accountId: config.require("accountId"),
+    hostname: config.require("zoneName"),
+    service: script.name,
+    zoneId: config.require("zoneId"),
+});
+
+new cloudflare.WorkersRoute("route", {
+    zoneId: config.require("zoneId"),
+    pattern: pulumi.interpolate`${domain.hostname}/.well-known/webfinger`,
+    scriptName: script.name,
+});
+
+new cloudflare.WorkersSecret("identities", {
+    accountId: config.require("accountId"),
+    name: "IDENTITIES",
+    scriptName: script.name,
+    secretText: config.require("identities"),
+});
